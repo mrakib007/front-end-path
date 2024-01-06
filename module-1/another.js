28afafa60e49563515496889bd4475de34ce6b5a
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-01-06 20:20:52 +0600

    Started front end path and making react app manually

---
+++ module-1/another.js
@@ -0,0 +1,7 @@
+export const HelloWorld = () => {
+  return React.createElement("p", {}, "Hello World");
+};
+
+export const anotherFunction = () => {
+  return React.createElement("p", {}, "Another Function is bundling");
+};
