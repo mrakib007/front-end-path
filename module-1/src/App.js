81596472b7688c45d0a2179d330ee5d0dd8dbdce
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-01-07 11:38:24 +0600

    Added some scripts and other config packages to make a react app manually

---
+++ module-1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <>
+      <div>Hello World</div>
+      <Home />
+    </>
+  );
+};
+
+export default App;
--- module-1/src/main.js
+++ module-1/src/main.js
@@ -1,31 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
-import { HelloWorld, anotherFunction } from "./another";
+import { ReactDOM, createRoot } from "react-dom/client";
+import App from "./App";
 
-const App = () => {
-  return React.createElement(
-    "h1",
-    { style: { color: "red" } },
-    "This is a simple react app",
-    HelloWorld(),
-    anotherFunction()
-  );
-};
-
-// const HelloWorld = () => {
-//   return <h1>Hello World</h1>;
-// };
-
-// const App = () => {
-//   return (
-//     <React.Fragment>
-//       <div>This is a simple react app</div>
-//       <HelloWorld />
-//     </React.Fragment>
-//   );
-// };
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-root.render(React.createElement(App));
-console.log(root);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
