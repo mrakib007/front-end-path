44b18740c072484c84f93b53483f027cdfc5decb
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-01-14 22:27:42 +0600

    module 2 finished

--- react-hooks/src/pages/UseEffectExample.tsx
+++ react-hooks/src/pages/UseEffectExample.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 const UseEffectExample = () => {
-//   const [count, setCount] = useState(0);
-const [hidden, setHidden] = useState(false);
+  //   const [count, setCount] = useState(0);
+  const [hidden, setHidden] = useState(false);
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  useEffect(() => {
+    console.log("render");
+  }, [user.name,user.email]);
   // useEffect(()=>{
   //     console.log('Render');
 
@@ -11,51 +16,59 @@ const [hidden, setHidden] = useState(false);
   //     }
   // },[hidden])
 
-//   useEffect(() => {
-//     setInterval(() => {
-//       // setCount(count+1)
-//       setCount((prev) => prev + 1);
-//     }, 1000);
-//   }, []);
+  //   useEffect(() => {
+  //     setInterval(() => {
+  //       // setCount(count+1)
+  //       setCount((prev) => prev + 1);
+  //     }, 1000);
+  //   }, []);
   return (
+    //     <div>
+    //  <button onClick={()=> setHidden((prev)=> !prev)}>{hidden ? 'Show' : 'Hide'}</button>
+    //       {/* <h1>{count}</h1> */}
+    //       {!hidden && <Counter/>}
+    //     </div>
     <div>
- <button onClick={()=> setHidden((prev)=> !prev)}>{hidden ? 'Show' : 'Hide'}</button> 
-      {/* <h1>{count}</h1> */}
-      {!hidden && <Counter/>}
+      <input onBlur={(e) => setUser({...user,name: e.target.value})}
+      type='text'
+      name="name"
+      id="nae"/>
+      <input onBlur={(e) => setUser({...user,email: e.target.value})}
+      type='text'
+      name="email"
+      id="nae"/>
     </div>
   );
 };
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-       const intervalId =  setInterval(() => {
-          // setCount(count+1)
-          setCount((prev) => prev + 1);
-        }, 1000);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // setCount(count+1)
+      setCount((prev) => prev + 1);
+    }, 1000);
 
-        return () =>{
-            clearInterval(intervalId)
-        }
-      }, []);
-    return  <h1>{count}</h1>
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return <h1>{count}</h1>;
 };
 
 const Todo = () => {
   const controller = new AbortController();
   const signal = controller.signal; //property
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1',{signal})
-    .then(res => res.json())
-    .then(data => console.log(data));
+    fetch("https://jsonplaceholder.typicode.com/todos/1", { signal })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
 
-    return () =>{
+    return () => {
       controller.abort(); //method
-    }
+    };
   }, []);
-  return <div>Todo</div> //9 minutes 50 seconds
-}
+  return <div>Todo</div>;
+};
 
 export default UseEffectExample;
-
-
