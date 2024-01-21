360328032f1a315049f6f66c4f30fceb304cfd35
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-01-21 22:17:19 +0600

    learning about reusable components

--- react-reusable-components/src/App.tsx
+++ react-reusable-components/src/App.tsx
@@ -1,7 +1,15 @@
 import MainLayout from "./components/layout/MainLayout";
+import Container from "./components/ui/Container";
 
 function App() {
 
-  return <MainLayout />
+  return (
+   <Container>
+    <div className="h-screen w-full flex justify-center items-center">
+      <button className="px-3 py-2 bg-purple-500 rounded-md">This is a button</button>
+      <button className="px-3 py-2 bg-purple-500 rounded-md">This is a button</button>
+    </div>
+   </Container>
+  );
 }
 export default App
