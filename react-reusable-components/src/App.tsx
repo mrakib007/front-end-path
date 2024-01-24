ba886ae77adf62207d80b8b7b73ee88c284c449d
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-01-24 12:57:30 +0600

    Reusable button learning

--- react-reusable-components/src/App.tsx
+++ react-reusable-components/src/App.tsx
@@ -5,16 +5,18 @@ import Container from "./components/ui/Container";
 function App() {
 
   return (
-   <Container>
-    <div className="h-screen w-full flex justify-center items-center">
-      {/* <h1>This is a h1 tag</h1>
+    <Container>
+      <div className="h-screen w-full flex justify-center items-center">
+        {/* <h1>This is a h1 tag</h1>
       <h2>This is a h1 tag</h2>
       <h3>This is a h1 tag</h3>
       <button className="btn btn-primary">This is a button</button>
       <button className="btn py-2 btn-danger">This is a button</button> */}
-      <Button variant="solid" className={"w-[500px]"}>Click</Button>
-    </div>
-   </Container>
+        <div className="w-96 border border-red-500 p-10">
+          <Button variant="outline" className="w-full">Click</Button>
+        </div>
+      </div>
+    </Container>
   );
 }
 export default App
--- react-reusable-components/src/components/ui/Button.tsx
+++ react-reusable-components/src/components/ui/Button.tsx
@@ -6,13 +6,21 @@ import cn from '../../utils/cn';
 // `bg-purple-500 px-3 py-2 rounded-md ${className}
 // ${variant == "outline" ? "border border-purple-500 bg-opacity-10" : ""}`
 
-const Button = ({ className, outline }) => {
+const Button = ({ className, variant }) => {
+    const getVariant = () => {
+        switch(variant) {
+            case "outline":
+                return "btn-outline";
+            case "ghost":
+                return "btn-ghost";
+            default:
+                return "btn-solid";
+        }
+    };
     return (
         <button
-            className={cn("bg-purple-500 px-3 py-2 rounded-md",
-                {
-                    "border border-purple-500 bg-opacity-10": outline,
-                },
+            className={cn(
+                getVariant(variant),
                 className)}>
             Click
         </button>
