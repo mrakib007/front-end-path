50dcc27c4e587afec7433b899739665a02336c98
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-01-26 13:00:20 +0600

    Creating a form

--- react-reusable-components/src/components/NormalForm/NormalForm.tsx
+++ react-reusable-components/src/components/NormalForm/NormalForm.tsx
@@ -11,10 +11,27 @@ const NormalForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}> 
-           <label htmlFor="name">Name</label>
-           <input type="text" id="name" {...register('name')}/>
-           <button type="submit">Submit</button>
+        <form onSubmit={handleSubmit(onSubmit)} className="max-w-5xl
+        p-5 mx-auto border border-red-500">
+            <div className="border border-green-500 grid grid-cols-2 gap-5">
+                <div className="w-full">
+                    <label className="block" htmlFor="name">Name</label>
+                    <input className="w-full" type="text" id="name" {...register('name')} />
+                </div>
+                <div className="w-full">
+                    <label className="block" htmlFor="name">Email</label>
+                    <input className="w-full" type="text" id="name" {...register('name')} />
+                </div>
+                <div className="w-full">
+                    <label className="block" htmlFor="name">Password</label>
+                    <input className="w-full" type="text" id="name" {...register('name')} />
+                </div>
+            </div>
+            {/* <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select> */}
+            {/* <button type="submit">Submit</button> */}
         </form>
     );
 };
