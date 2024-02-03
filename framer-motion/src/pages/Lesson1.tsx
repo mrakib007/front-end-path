0ca96c79dc1492b6d45e88df778ffb98b6437486
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-02-03 12:52:46 +0600

    gesture tap ,drag

--- framer-motion/src/pages/Lesson1.tsx
+++ framer-motion/src/pages/Lesson1.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { easeInOut, motion } from 'framer-motion';
 
 const parent = {
     hidden: { opacity: 0, scale: 0.9 },
     visible: {
-        opacity: 1, scale: 1, transition: {
+            opacity: 0.6,
+            scale: 1, 
+            transition: {
             ease: 'easeInOut',
-            duration: 2,
-            delayChildren: 0.5,
-            staggerChildren: 0.5
+            duration: 0.5,
+            // delayChildren: 0.5,
+            // staggerChildren: 0.5
         }
     },
     hover: {
-        scale: 1,
-        transition: {
-            duration: 0.5
-        }
+        opacity: 1
     },
     tap: {
-        scale: 1,
-        rotate: 45
+        scale: 1.1,
+        boxShadow: '0px 0px 8px 8px #000'
     }
 }
 
@@ -29,17 +28,22 @@ const parent = {
 // }
 
 const Lesson1 = () => {
+    const parentRef = useRef(null);
     return (
-        <div>
+        <div ref={parentRef} className='border border-red-500 size-[700px] flex justify-center items-center'>
             <motion.div
                 className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center items-center"
                 variants={parent}
                 initial='hidden'
                 animate='visible'
                 whileHover='hover'
+                // whileTap='tap'
+                drag
+                // dragSnapToOrigin
+                dragElastic={0.7}
+                dragConstraints={parentRef}
                 whileTap='tap'
-                onHoverStart={()=> console.log('hover start')}
-                onHoverEnd={()=> console.log('hover end')}
+                // dragConstraints={{left: -200,right: 200, top : -200, bottom: 200}}
             >
             </motion.div>
         </div>
