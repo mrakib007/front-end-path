46418642e7ff31b274d1452f3dd374d3db52f2f2
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-02-10 17:05:17 +0600

    useCycle() hook

--- framer-motion/src/App.jsx
+++ framer-motion/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
+import Lesson from './pages/Lesson'
 // import Lesson1 from './pages/lesson1'
-import Lesson2 from './pages/Lesson2'
-import Lesson3 from './pages/Lesson3'
+// import Lesson2 from './pages/Lesson2'
+// import Lesson3 from './pages/Lesson3'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <>
     {/* <Lesson1/> */}
     {/* <Lesson2 />  */}
-    <Lesson3/>
+    {/* <Lesson3/> */}
+    <Lesson />
     </>
   )
 }
---
+++ framer-motion/src/pages/Lesson.jsx
@@ -0,0 +1,23 @@
+import { motion, useCycle } from 'framer-motion';
+
+const Lesson = () => {
+    const boxAnimation = [
+            {x: 0 , y: 0, opacity: 1},
+            {x: 100 , y: 100, opacity: 0.5},
+            {x: -100 , y: 50, opacity: 0.25},
+            {x: -100 , y: -100, opacity: 0},
+        ]
+    
+    const [animate, cycle] = useCycle(...boxAnimation);
+    return (
+        <div className='border border-red-500 size-[600px] flex flex-col justify-center'>
+            <motion.div className='size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5'
+            animate={animate}
+            onTap={()=> cycle()}>
+
+            </motion.div>
+        </div>
+    );
+};
+
+export default Lesson;
