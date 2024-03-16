15c0962c45cc7c3f4b610a0be6352194d69d7431
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-03-16 18:09:15 +0600

    Shad cn installation and configuration

---
+++ module-8/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div>React + Tailwind + RRD</div>
+      <Button>Something</Button>
+    </>
+  )
+}
+
+export default App
