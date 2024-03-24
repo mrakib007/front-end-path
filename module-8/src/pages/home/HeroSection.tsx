03f6eec25774ecb67e700927a0eea4061b3a57ca
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-03-24 20:25:17 +0600

    responsive hero section

--- module-8/src/pages/home/HeroSection.tsx
+++ module-8/src/pages/home/HeroSection.tsx
@@ -4,24 +4,24 @@ import macbook from "@/assets/images/macbook-exposed.png"
 
 const HeroSection = () => {
     return (
-        <Container className="h-[calc(100vh-64px)] grid grid-cols-2 place-content-center">
+        <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
             <div>
-                <h1 className="text-8xl font-bold">
+                <h1 className="text-5xl lg:text-8xl font-bold text-nowrap">
                     <span className="text-gray">Don't worry,</span>
                     <br />
                     <span>We'll fix it</span>
                 </h1>
-                <p>
-                    Welcome to <span>iRepair</span>,
+                <p className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
+                    Welcome to <span className="text-primary-foreground font-semibold">iRepair</span>,
                     your one-stop place for all kinds of
-                    <span>Macbook repairs</span> and diagnostics.
+                    <span className="text-primary-foreground font-semibold">Macbook repairs</span> and diagnostics.
                 </p>
                 <Button>
                     Book a service
                 </Button>
             </div>
-            <div>
-                <img src={macbook} alt="" srcSet="" />
+            <div className="mt-10 w-3/4 lg:w-full mx-auto">
+                <img className="-rotate-[35deg] h-[95%] object-contain" src={macbook} alt="" srcSet="" />
             </div>
         </Container>
     );
