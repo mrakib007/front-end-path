3f691948e656be4d20269396d9ccf4a00d5d04bb
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-03-26 16:37:33 +0600

    navbar with animation

--- module-8/src/components/layouts/NavBar.tsx
+++ module-8/src/components/layouts/NavBar.tsx
@@ -5,10 +5,10 @@ import { motion } from 'framer-motion';
 const NavBar = () => {
     return (
         <motion.header
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-            className="h-16">
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{delay:1,duration:1}}
+         className="h-16 fixed w-full bg-white z-[999]">
             <nav className="w-full h-full px-[20px] max-w-[1230px] mx-auto flex
             justify-between items-center">
                 <span className="text-3xl">iRepair</span>
