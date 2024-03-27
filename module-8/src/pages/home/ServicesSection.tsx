c955f2b007828097988b75391f982bca6b913a2e
mrakib007 <mrakibhasan199744@gmail.com>
2024-03-27 11:56:27 +0600

    Scroll based transition

--- module-8/src/pages/home/ServicesSection.tsx
+++ module-8/src/pages/home/ServicesSection.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/Container";
+import BatteryReplacement from "@/components/ServiceTiles/BatteryReplacement";
 
 const ServicesSection = () => {
     return (
@@ -11,13 +12,13 @@ const ServicesSection = () => {
                 </p>
             </div>
             <div className="grid grid-cols-12 gap-[20px]">
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-12"></div>
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-6 lg:col-span-5"></div>
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-6 lg:col-span-7"></div>
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-12"></div>
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-12 md:col-span-6 lg:col-span-4"></div>
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-12 md:col-span-6 lg:col-span-4"></div>
-            <div className="bg-light-gray h-[415px] rounded-2xl col-span-12 md:col-span-12 lg:col-span-4"></div>
+               <BatteryReplacement/>
+                <div className="bg-light-gray h-[415px] rounded-2xl col-span-6 lg:col-span-5"></div>
+                <div className="bg-light-gray h-[415px] rounded-2xl col-span-6 lg:col-span-7"></div>
+                <div className="bg-light-gray h-[415px] rounded-2xl col-span-12"></div>
+                <div className="bg-light-gray h-[415px] rounded-2xl col-span-12 md:col-span-6 lg:col-span-4"></div>
+                <div className="bg-light-gray h-[415px] rounded-2xl col-span-12 md:col-span-6 lg:col-span-4"></div>
+                <div className="bg-light-gray h-[415px] rounded-2xl col-span-12 md:col-span-12 lg:col-span-4"></div>
             </div>
         </Container>
     );
