6c8c5a8aef6f306c37fc678849b81b735bad1709
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-03-30 12:30:39 +0600

    Responsive sidebar

--- module-8/src/components/layouts/SideBar.tsx
+++ module-8/src/components/layouts/SideBar.tsx
@@ -1,13 +1,32 @@
+import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 const SideBar = () => {
     return (
-        <aside className="bg-light-gray col-span-2 h-screen sticky top-0 left-0 overflow-auto p-5">
-            <nav className="flex flex-col ">
-                <NavLink to="/admin" className="p-3 bg-gray rounded-md hover:bg-dark-gray
-                 hover:text-white transition-all truncate">Dashboard</NavLink>
-                <NavLink to="/admin/add-service">Add Service</NavLink>
-                <NavLink to="/admin/service-list">Service List</NavLink>
+        <aside className="bg-light-gray col-span-2 h-screen sticky top-0 left-0 overflow-auto p-4 lg:p-5">
+            <nav className="flex flex-col gap-2">
+                <NavLink to="/admin/dashboard" className={({ isActive }) => cn("p-3 bg-gray hover:bg-dark-gray hover:text-white rounded-md transition-all flex item-center gap-2",
+                    {
+                        "bg-dark-gray text-white": isActive
+                    })}>
+                    <LayoutDashboard className="shrink-0" />
+                    <span className="truncate">Dashboard</span>
+                </NavLink>
+                <NavLink to="/admin/add-service" className={({ isActive }) => cn("p-3 bg-gray hover:bg-dark-gray hover:text-white rounded-md transition-all flex item-center gap-2",
+                    {
+                        "bg-dark-gray text-white": isActive
+                    })}>
+                    <LayoutDashboard className="shrink-0" />
+                    <span className="truncate">Add Service</span>
+                </NavLink>
+                <NavLink to="/admin/service-list" className={({ isActive }) => cn("p-3 bg-gray hover:bg-dark-gray hover:text-white rounded-md transition-all flex item-center gap-2",
+                    {
+                        "bg-dark-gray text-white": isActive
+                    })}>
+                    <LayoutDashboard className="shrink-0" />
+                    <span className="truncate">Service List</span>
+                </NavLink>
             </nav>
         </aside>
     );
