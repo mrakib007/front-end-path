b16b07d605a9581228c29904ce3c7f98cc1579ff
mrakib007 <mrakibhasan199744@gmail.com>
2024-03-31 09:35:18 +0600

    tanstack query setup

--- module-8/src/pages/admin/ServiceList.tsx
+++ module-8/src/pages/admin/ServiceList.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 const ServiceList = () => {
+    const [data,setData] = useState([]);
+    const [error,setError] = useState(null);
+    const [loading,isLoading] = useState(false);
+    const getData = async () =>{
+
+    }
+    useEffect(()=>{
+        fetch('http://localhost:5000/api/v1/services')
+        .then(res => res.json())
+        .then(data => setData(data?.data)) 
+    },[])
     return (
         <div>
             
