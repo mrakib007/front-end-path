acf6836a820cefac0dbe86f363cebcc7ff0a54ee
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-01 09:43:28 +0600

    tanstack query and new api folder structure

---
+++ module-8/src/api/admin/services/service.api.ts
@@ -0,0 +1,4 @@
+export const getServices = async () => {
+    return await fetch('http://localhost:5000/api/v1/services')
+    .then(res => res.json())
+}
--- module-8/src/pages/admin/ServiceList.tsx
+++ module-8/src/pages/admin/ServiceList.tsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react";
+import {useQuery} from '@tanstack/react-query';
+import { getServices } from "@/api/admin/services/service.api";
 
 const ServiceList = () => {
-    const [data,setData] = useState([]);
-    const [error,setError] = useState(null);
-    const [loading,isLoading] = useState(false);
-    const getData = async () =>{
+    // const [data,setData] = useState([]);
+    // const [error,setError] = useState(null);
+    // const [loading,isLoading] = useState(false);
+    // const getData = async () =>{
 
+    // }
+    // useEffect(()=>{
+    //     fetch('http://localhost:5000/api/v1/services')
+    //     .then(res => res.json())
+    //     .then(data => setData(data?.data)) 
+    // },[])
+
+   
+
+    const {data,isLoading,isError} = useQuery({
+        queryKey: ['services'],
+        queryFn: getServices,
+    })
+    if(isLoading){
+        return <p>Loading...</p>
     }
-    useEffect(()=>{
-        fetch('http://localhost:5000/api/v1/services')
-        .then(res => res.json())
-        .then(data => setData(data?.data)) 
-    },[])
+    console.log(data,isLoading,isError);
     return (
         <div>
-            
+            {
+                data?.data?.map(item=><h1>
+                    {item.name}
+                </h1>)
+            }
         </div>
     );
 };
