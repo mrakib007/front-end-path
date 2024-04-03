e8c4bb4007041de46d37e0776b8e1a9876bb821a
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-03 20:06:59 +0600

    table and data fetching with select

--- module-8/src/components/layouts/AdminLayout.tsx
+++ module-8/src/components/layouts/AdminLayout.tsx
@@ -5,7 +5,7 @@ const AdminLayout = () => {
     return (
         <div className="grid grid-cols-12">
             <SideBar/>
-            <div className="col-span-10 h-[200vh]">
+            <div className="col-span-10 h-full px-5">
             <Outlet/>
             </div>
         </div>
--- module-8/src/pages/admin/ServiceList.tsx
+++ module-8/src/pages/admin/ServiceList.tsx
@@ -1,5 +1,9 @@
-import {useQuery} from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { getServices } from "@/api/admin/services/service.api";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+import Container from '@/components/Container';
+import { Trash2 } from 'lucide-react';
 
 const ServiceList = () => {
     // const [data,setData] = useState([]);
@@ -14,28 +18,69 @@ const ServiceList = () => {
     //     .then(data => setData(data?.data)) 
     // },[])
 
-   
 
-    const {data,isLoading,isError} = useQuery({
+
+    const { data: services, isLoading, isError } = useQuery({
         queryKey: ['services'],
         queryFn: getServices,
+        select: (data) => {
+            const services = data?.data?.map(item => ({
+                id: item._id,
+                name: item.name,
+                description: item.description,
+                price: item.price
+            }));
+            return services;
+        }
     })
-    if(isLoading){
+    if (isLoading) {
         return <p>Loading...</p>
     }
-    console.log(data,isLoading,isError);
-    
-    if(isError){
+
+    if (isError) {
         return <p>Something went wrong</p>
     }
+
+    // const services = data?.data?.map(item => ({
+    //     id: item._id,
+    //     name: item.name,
+    //     description: item.description,
+    //     price: item.price
+    // }))
     return (
-        <div>
-            {
-                data?.data?.map(item=><h1>
-                    {item.name}
-                </h1>)
-            }
-        </div>
+        <Container className='mt-20 border p-0 rounded-2xl'>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Description</TableHead>
+                        <TableHead>Price</TableHead>
+                        <TableHead className="text-right">Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {services.map((service) => (
+                        <TableRow key={service.id}>
+                            <TableCell className="font-medium">{service.name}</TableCell>
+                            <TableCell>{service.description}</TableCell>
+                            <TableCell>{service.price}</TableCell>
+                            <TableCell className="text-right">
+                                <Button variant="destructive" className='p-2'>
+                                    <Trash2 />
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={3}>Total Services</TableCell>
+                        <TableCell className="text-right">{services.length}</TableCell>
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </Container>
     );
 };
 
