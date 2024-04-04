4f1cf867f12aed7b6a3df59627743f07934e71bf
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-04 23:59:09 +0600

    Post data with tanstack query

--- module-8/src/pages/admin/AddService.tsx
+++ module-8/src/pages/admin/AddService.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { FormEvent, useState } from "react";
+
 const AddService = () => {
+    const [serviceName,setServiceName] = useState('');
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const serviceData = {
+            name: serviceName,
+            description: 'Replace any dead chips',
+            devices: ['Macbook Pro', 'Macbook Air', 'Macbook', 'iMac', 'Mac Pro', 'Mac Mini'],
+            price: 1000,
+        }
+        console.log(serviceData);
+    }
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={(e)=>setServiceName(e.target.value)}/>
+                <Button type="submit">Submit</Button>
+            </form>
         </div>
     );
 };
