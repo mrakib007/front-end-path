84a1502c0643e3433a1a8bbb47e0f5e3b1c7fe6d
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-08 23:38:44 +0600

    basic counter with redux

--- redux-learning/redux-module-1/src/App.tsx
+++ redux-learning/redux-module-1/src/App.tsx
@@ -1,11 +1,15 @@
-function App() {
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "./redux/features/counterSlice";
 
+function App() {
+  const {count} = useSelector((state)=> state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
-        <button className="text-white px-3 py-2 rounded-md bg-green-500 text-xl font-semibold">Increment</button>
-        <h1 className="text-3xl mx-10">0</h1>
-        <button className="text-white px-3 py-2 rounded-md bg-red-500 text-xl font-semibold">Decrement</button>
+        <button onClick={()=>dispatch(increment())} className="text-white px-3 py-2 rounded-md bg-green-500 text-xl font-semibold">Increment</button>
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button onClick={()=>dispatch(decrement())} className="text-white px-3 py-2 rounded-md bg-red-500 text-xl font-semibold">Decrement</button>
       </div>
     </div>
   )
