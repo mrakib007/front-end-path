be3c2e48ff7356286681ffa8680756209bc8ac80
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-08 23:19:44 +0600

    Initializing store and slice

--- redux-learning/redux-module-1/src/App.tsx
+++ redux-learning/redux-module-1/src/App.tsx
@@ -1,11 +1,13 @@
-
-
 function App() {
 
   return (
-    <>
-    <h1>react with redux</h1>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
+        <button className="text-white px-3 py-2 rounded-md bg-green-500 text-xl font-semibold">Increment</button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="text-white px-3 py-2 rounded-md bg-red-500 text-xl font-semibold">Decrement</button>
+      </div>
+    </div>
   )
 }
 
