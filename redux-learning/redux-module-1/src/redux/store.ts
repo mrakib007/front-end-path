6e77c07c3bb3c31c02d321a9462523a83e960376
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-10 18:08:01 +0600

    DevTool,middleware,custom middlewares

--- redux-learning/redux-module-1/src/redux/store.ts
+++ redux-learning/redux-module-1/src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './features/counterSlice';
+import Logger from "./middleware/logger";
+// import logger from 'redux-logger';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-    }
+    },
+    devTools: false,
+    // middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(logger)
+    middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(Logger)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
