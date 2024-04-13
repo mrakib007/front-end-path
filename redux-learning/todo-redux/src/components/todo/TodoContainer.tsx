cba153e9ad4eb426985fa3bc5675dc64f9edb1a5
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-13 09:35:46 +0600

    making todo components with shadcn

--- redux-learning/todo-redux/src/components/todo/TodoContainer.tsx
+++ redux-learning/todo-redux/src/components/todo/TodoContainer.tsx
@@ -1,13 +1,20 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
     return (
         <div>
-            <div>
-                <button>Add todo</button>
+            <div className="flex justify-between mb-5">
+                <AddTodoModal />
                 <button>Filter</button>
             </div>
-            <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
+            <div className="bg-primary-gradient p-[5px] w-full h-full rounded-xl">
+                <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+                    <TodoCard />
+                    <TodoCard />
+                    <TodoCard />
+                </div>
                 {/* <div className="bg-white flex justify-center items-center rounded-md p-5 text-2xl font-bold">
                     <p>There is no tas pending</p>
                 </div> */}
