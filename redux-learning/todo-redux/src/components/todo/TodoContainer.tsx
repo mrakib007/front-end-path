7e8b21284d7eb1bc3066cd4a467e6ee637c5939e
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-06-17 13:28:33 +0600

    Set up for query

--- redux-learning/todo-redux/src/components/todo/TodoContainer.tsx
+++ redux-learning/todo-redux/src/components/todo/TodoContainer.tsx
@@ -2,19 +2,26 @@ import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-    const {todos} = useAppSelector((state) => state.todos);
+    //From Local State.
+    const { todos } = useAppSelector((state) => state.todos);
+
+    //From server
+    const { data, isLoading, isError } = useGetTodosQuery(undefined);
+
+    if(isLoading) return <p>Loading...</p>
     return (
         <div>
             <div className="flex justify-between mb-5">
                 <AddTodoModal />
-                <TodoFilter/>
+                <TodoFilter />
             </div>
             <div className="bg-primary-gradient p-[5px] w-full h-full rounded-xl">
                 <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
                     {
-                        todos.map((item)=><TodoCard {...item}/>)
+                        todos?.data?.map((item) => <TodoCard {...item} />)
                     }
                 </div>
                 {/* <div className="bg-white flex justify-center items-center rounded-md p-5 text-2xl font-bold">
--- redux-learning/todo-redux/src/redux/store.ts
+++ redux-learning/todo-redux/src/redux/store.ts
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from './features/todoSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./features/todoSlice";
+import { baseApi } from "./api/api";
 export const store = configureStore({
-    reducer: {
-        todos: todoReducer,
-    },
+   reducer: {
+      [baseApi.reducerPath]: baseApi.reducer,
+      todos: todoReducer,
+   },
+   middleware: (getDefaultMiddlewares) =>
+      getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
